774a287d1c9a0f2ae52b05cd5138b56f3f14d119
Aswath-77 <aswatha12345s@gmail.com>
2026-02-25 21:26:17 +0530

    change

--- My_Portfolio.js
+++ My_Portfolio.js
@@ -1,4 +1,5 @@
 //  ===================================
+// import { useState } from 'react';
 //     Skills Progress Animation
 //     ===================================
     function animateSkillBars() {
@@ -28,3 +29,85 @@
     }
     
     animateSkillBars();
+
+
+
+
+
+
+    // Form validation =========================================================================
+
+
+    let id = (z) => document.getElementById(z);
+
+    let classes =  (classes) => document.getElementsByClassName(classes);
+
+
+    let username = id("username"),
+     email = id("email"),
+      subject = id("subject"),
+      message = id("message"),
+    //   form = id("form"),
+
+       errorMsg = classes("error"),
+       successIcon = classes("success-icon"),
+       failureIcon = classes("failure-icon");
+
+
+
+          form.addEventListener("submit", function(abc){
+         
+         abc.preventDefault(); 
+        //  console.log(abc)
+
+
+
+        // function call
+
+        engine(username, 0, "Username cannot be blank");
+        engine(email, 1, "Email cannot be blank");
+        engine(subject, 2, "subject cannot be blank");
+        engine(message, 3, "Message cannot be blank");
+
+          
+    });
+
+
+    let engine= (id, serial, message) => {
+
+           if(id.value.trim()==""){
+
+             errorMsg[serial].innerHTML = message; 
+             
+               id.style.border = " 1.5px solid red ";
+
+
+
+              // icons 
+               failureIcon[serial].style.opacity = "1";
+               successIcon[serial].style.opacity = "0";
+           }
+           else{
+
+            errorMsg[serial].innerHTML = ""; 
+
+            id.style.border = " 1.5px solid green ";
+
+           
+               // icons
+               failureIcon[serial].style.opacity = "0";
+               successIcon[serial].style.opacity = "1";
+               
+           }
+    }
+
+
+
+
+
+
+    
+
+
+
+    
